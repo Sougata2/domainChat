43faefd3106078191196fa74b82f5907fa8b90a5
sougata <sougata19582001@gmail.com>
2026-01-18 19:11:22 +0530

    added enable Async and enableScheduling and configuration scan

--- src/main/java/com/domain/chat/ChatApplication.java
+++ src/main/java/com/domain/chat/ChatApplication.java
@@ -2,9 +2,15 @@ package com.domain.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {
         "com.domain.chat",
         "com.domain.mapper",
