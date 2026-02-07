8b0c9be44ab192b5b6b5fb5296346ab2b1505722
sougata <sougata19582001@gmail.com>
2026-02-07 23:42:39 +0530

    added a controller to view/download media.

--- src/main/java/com/domain/chat/app/media/controller/MediaController.java
+++ src/main/java/com/domain/chat/app/media/controller/MediaController.java
@@ -1,13 +1,21 @@
 package com.domain.chat.app.media.controller;
 
 import com.domain.chat.app.media.dto.MediaDto;
+import com.domain.chat.app.media.properties.MediaProperties;
 import com.domain.chat.app.media.service.MediaService;
 import com.domain.chat.app.message.dto.MessageDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -15,6 +23,7 @@ import java.util.List;
 @RequestMapping("/media")
 public class MediaController {
     private final MediaService service;
+    private final MediaProperties properties;
 
     @PostMapping("/send-media")
     public ResponseEntity<MessageDto> sendMedia(
@@ -33,4 +42,26 @@ public class MediaController {
     public ResponseEntity<MessageDto> send(@RequestBody MessageDto dto) {
         return ResponseEntity.ok(service.send(dto));
     }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> getFile(
+            @PathVariable String filename,
+            @RequestParam(required = false) boolean download
+    ) throws IOException {
+        Path filePath = Paths.get(properties.getUploadDirectory()).resolve(filename);
+        Resource resource = new UrlResource(filePath.toUri());
+
+        if (!resource.exists()) throw new RuntimeException("File not found");
+
+        String contentType = Files.probeContentType(filePath);
+        if (contentType == null) contentType = "application/octet-stream";
+
+        String disposition = download ? "attachment" : "inline";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header("Content-Disposition", disposition + "; filename=\"" + filename + "\"")
+                .body(resource);
+    }
+
 }
